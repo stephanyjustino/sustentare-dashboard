4d81f2d1a11af827df9972a34afe0ccebd82cc4e
RaphaelKlein <125585619+RaphaelKlein@users.noreply.github.com>
2024-08-25 21:14:58 -0300

    CorreçÕes e componente de KPI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-const root = ReactDOM.createRoot(document.getElementById('desktopRoot'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
