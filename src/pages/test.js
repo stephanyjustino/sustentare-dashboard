fbbe1f17bdb8f166a62c614cfe4d934c7b5b47a4
PereiraGus <gustavo.oito@hotmail.com>
2024-09-21 10:50:47 -0300

    8 - Adições no CSS global e limpeza de código.

--- src/pages/test.js
+++ src/pages/test.js
@@ -3,7 +3,7 @@ import CheckableList from "../components/CheckableList/CheckableList";
 export default function Teste (){
 
     return (
-        <div>
+        <div style={{width: '300px'}}>
             <CheckableList/>
         </div>
     );
